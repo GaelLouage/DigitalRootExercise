5111a3effc8800545d9887f625400f7c4e2780e1
Gaël Louage <gaellouage@hotmail.com>
2025-06-03 14:53:15 +0200

    DigitalRootexercise

---
+++ DigitalRootExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DigitalRoot(16999));
+    }
+    public static int DigitalRoot(long n)
+    {
+        var result = long.Parse(n.ToString());
+        var resString = result.ToString();
+        var counter = 0;
+        var data = 0;
+        while (resString.Length > 1)
+        {
+            data += (int)char.GetNumericValue(resString[counter]);
+
+            if (counter == result.ToString().Length - 1)
+            {
+                counter = 0;
+                result = data;
+                resString = result.ToString();
+                data = 0;
+            }
+            else
+            {
+                counter++;
+            }
+        }
+        return (int)result;
+    }
+}
